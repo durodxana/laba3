63523a2ff003a0e783b72263f899a22bb481492b
Navruz Ergashev <durodxana@mail.ru>
2016-05-13 03:53:56 +0300

    второй коммит для выполнение лабораторной работы №3

--- src/laba3.java
+++ src/laba3.java
@@ -32,7 +32,32 @@ public class laba3 {
     private static String filename = "C:\\Users\\Student\\Desktop\\slova.txt";
 
 
-    public static void main(String[] args){
+    public static void main (String [] args ) throws FileNotFoundException {
+        String textFromFile = read (filename);
+        System.out.println(textFromFile);
+        Pattern p = Pattern.compile ("\\b[А-я]+(у|ишь|[ая]ть|[ен]т|и[тм]|[ну]т|[и]те|ся)\\b");
+        Matcher m = p.matcher ( textFromFile);
+        int k = 0;
+        while (m.find()) {
+            System.out.println(m.group());
+            k++;
+
+        }
+
+        System.out.println("________");
+        System.out.println("Кол-во глаголов:" +k);
+        System.out.println();
+
+
+        Pattern nar = Pattern.compile("\\b[в|из|до|на|за][А-я]+[оа]\\b");
+        Matcher m_nar = nar.matcher(textFromFile);
+        int k_nar = 0 ;
+        while (m_nar.find())
+        {
+            System.out.println(m_nar.group());
+            k_nar++;
+        }
+
 
     }
 }
